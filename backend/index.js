004047110b57772503d7ac4f098f8adffaaca0d4
Rajiv Chaulagain <82865638+rajivchaulagain@users.noreply.github.com>
2022-10-16 14:25:42 +0545

    new 1

--- backend/index.js
+++ backend/index.js
@@ -24,16 +24,15 @@ app.use('/', express.static('public'));
 
 if (process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static(path.join(__dirname, 'client')));
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "client/build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
     });
 }
 
 app.use(cors());
 
 app.use(express.urlencoded({ extended: false }))
-
 
 app.get('/', (req, res) => {
     res.send(`this is home page`);
